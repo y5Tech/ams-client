6635042e3a26c3382323cc6441ba60317fa375fe
Yasin Efem Dalkılıç <ysndlklc1234@gmail.com>
2020-09-01 12:18:10 +0300

    initial commit

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,13 +1,69 @@
 import React from "react"
-import { Layout } from 'antd';
-const { Header } = Layout;
+import './index.css'
+import { Layout, Tabs } from 'antd';
 
-const AppHeader=()=>{
-    return(
-        <Header className="site-layout-background" style={{ padding: 0 }} >
-        Header
-        </Header>
-    )
+import { Menu } from 'antd';
+import HeaderDropdown from "../HeaderDropdown";
+import { DownOutlined } from '@ant-design/icons';
+import { Helmet } from "react-helmet";
+const { Header } = Layout;
+const { TabPane } = Tabs;
+
+const menu = (
+  <Menu>
+    <Menu.Item></Menu.Item>
+    <Tabs defaultActiveKey="2">
+      <TabPane
+        tab={
+          <span>
+            Tab 1
+        </span>
+        }
+        key="1"
+      >
+        Tab 1
+    </TabPane>
+      <TabPane
+        tab={
+          <span>
+            Tab 2
+        </span>
+        }
+        key="2"
+      >
+        Tab 2
+    </TabPane>
+    </Tabs>
+  </Menu>
+);
+const AppHeader = () => {
+  return (
+    <>
+      <Helmet>
+        <meta charSet="utf-8" />
+        <title>My Title</title>
+        <link rel="canonical" href="http://mysite.com/example" />
+      </Helmet>
+      <Header className="site-layout-background" style={{ padding: 0 }} >
+        <div className={"header-wrapper"}>
+          <div className="header-left">
+
+          </div>
+          <div className="header-right">
+            <HeaderDropdown overlay={menu}>
+              <div className="header-dropdown-link-container">
+                <a className="ant-dropdown-link" onClick={e => e.preventDefault()}>
+                  Yasin Efem Dalkılıç  <DownOutlined />
+                </a>
+              </div>
+            </HeaderDropdown>
+
+          </div>
+        </div>
+      </Header>
+
+    </>
+  )
 }
 
-export default  React.memo(AppHeader);
+export default React.memo(AppHeader);
---
+++ src/components/Tabs/index.tsx
@@ -0,0 +1,15 @@
+import React from "react"
+import { Tabs } from 'antd';
+
+interface PropTypes{
+    
+}
+
+const AppTabs=()=>{
+    return(
+        <Tabs defaultActiveKey="2">
+
+      </Tabs>
+    )
+}
+export default React.memo(AppTabs)

b6b3b87d6007016af106c0e24cea005842b67818
Yasin Efem Dalkılıç <ysndlklc1234@gmail.com>
2020-10-06 10:08:21 +0300

    token kontrolu duzenlendi

--- src/components/ProtectedRouter/index.tsx
+++ src/components/ProtectedRouter/index.tsx
@@ -1,27 +1,35 @@
 import {Redirect, Route} from "react-router-dom";
 import React, {useContext, useEffect, useState} from "react";
 import { ApplicationUtils } from "../../utils";
+import useLocalStorage from "../../customHooks/useLocalStorage";
 
 const ProtectedRoute = ({component: Component, ...rest}:any) => {
-    const [isAuth, setIsAuth] = useState(null)
+    const {getLocalStorage} = useLocalStorage()
+
+    const [isAuth, setIsAuth] = useState(false)
+    const [authDone, setAuthDone] = useState('')
     useEffect(() => {
-        const token = localStorage.getItem('token');
+          const token = getLocalStorage('token')
+
         if (token !== undefined && token !== null) {
             try {
                 let validToken = ApplicationUtils.parseJwt(token)
+                setAuthDone('ok')
                 setIsAuth(true)
             } catch (e) {
+                setAuthDone('ok')
                 setIsAuth(false)
             }
         } else {
+            setAuthDone('ok')
             setIsAuth(false)
         }
     }, [])
   
     return (
         <Route {...rest} render={(props) => {
-            return ((isAuth !== null && isAuth === true) ? <Component {...props}/> :
-                    (isAuth !== null && isAuth === false) ?
+            return ((authDone !== '' && isAuth === true) ? <Component {...props}/> :
+                    (authDone !== '' && isAuth === false) ?
                         <Redirect
                             to={{
                                 pathname: "/login",
